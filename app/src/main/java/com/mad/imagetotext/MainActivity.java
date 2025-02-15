f3b86b929c21a2880420237e2274c2de28fa3124
Kawindu <kawindu.dinindu@gmail.com>
2025-02-15 21:49:37 +0530

    Initial commit

---
+++ app/src/main/java/com/mad/imagetotext/MainActivity.java
@@ -0,0 +1,63 @@
+package com.mad.imagetotext;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.github.dhaval2404.imagepicker.ImagePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ImageView clear, getImage, copy;
+    EditText recogText;
+    Uri imageUri;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        clear = findViewById(R.id.clear);
+        getImage = findViewById(R.id.getImage);
+        copy = findViewById(R.id.copy);
+        recogText = findViewById(R.id.recogtext);
+
+        getImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImagePicker.with(MainActivity.this)
+                        .crop()
+                        .compress(1024)
+                        .maxResultSize(1080,1080)
+                        .start();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == Activity.RESULT_OK){
+            if (data != null){
+                imageUri = data.getData();
+            }
+        }
+        else{
+            Toast.makeText(this, "Image not Selected",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
